7d44370ff44a9bf47cc3fcfddfd5af729e0aeba4
Wesley Bonfim <wesleybbonfim@hotmail.com>
2026-06-09 01:03:12 -0300

    ci: ajustar DAST e aguardar aplicação subir

--- todo_project/run.py
+++ todo_project/run.py
@@ -4,5 +4,6 @@ import os
 if __name__ == '__main__':
     # Bind host controlled by environment to avoid hardcoded 0.0.0.0
     host = os.environ.get('APP_HOST', '127.0.0.1')
+    port = int(os.environ.get('APP_PORT', '5000'))
     debug = os.environ.get('FLASK_DEBUG', 'True').lower() in ('1', 'true', 'yes')
-    app.run(host=host, debug=debug)
+    app.run(host=host, port=port, debug=debug, use_reloader=False)
